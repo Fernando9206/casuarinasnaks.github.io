3db476edcdbafd54e071c739ad5492e09953dd7b
Luis Fernando Lupercio Ramirez <111186402+Fernando9206@users.noreply.github.com>
2026-02-20 08:07:13 -0600

    Add Firebase configuration and Firestore initialization

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCpdBpc6jH-iOUeRD4QRdkaqUSjmgIbmGU",
+    authDomain: "casuarinasnaks-menu.firebaseapp.com",
+    projectId: "casuarinasnaks-menu",
+    storageBucket: "casuarinasnaks-menu.firebasestorage.app",
+    messagingSenderId: "113253329955",
+    appId: "1:113253329955:web:236baea1897109c6f85596"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
